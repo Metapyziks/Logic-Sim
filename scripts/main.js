9b30e553741f259808aa489cd32d91abddbe2377
James King <metapyziks@gmail.com>
2013-10-31 15:06:13 +0000

    Wire placement should now be more efficient

--- scripts/main.js
+++ scripts/main.js
@@ -449,16 +449,8 @@ function LogicSim()
 			for (var i = 0; i < wires.length; ++ i) {
 				var wire = wires[i];
 
-				if (wire.isHorizontal()) {
-					if (wire.start.x < rect.right && wire.end.x > rect.left
-	                    && wire.start.y <= rect.bottom && wire.end.y >= rect.top) {
-						wire.selected = true;
-					}
-				} else {
-					if (wire.start.x <= rect.right && wire.end.x >= rect.left
-	                    && wire.start.y < rect.bottom && wire.end.y > rect.top) {
-						wire.selected = true;
-					}
+				if (rect.intersectsWire(wire)) {
+					wire.selected = true;
 				}
 			}
 		} else if (x < 256) {
